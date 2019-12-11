82f574631230f48f6876eddcdb37d4b001e65ea0
Matthew <matthewrule10@gmail.com>
2019-12-11 12:50:58 -0700

    portfolio

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/* Toggle between adding and removing the "responsive" class to topnav when the
+user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += "responsive";
   } else {
     x.className = "topnav";
   }
